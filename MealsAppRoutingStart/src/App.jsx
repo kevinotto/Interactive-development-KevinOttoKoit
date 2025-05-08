ca22846dd7222dbf1a134132ec4df21279c15e49
kevinotto <ottokoit@gmail.com>
2025-05-08 19:41:06 +0300

    Initial start of MealsAppRouting

---
+++ MealsAppRoutingStart/src/App.jsx
@@ -0,0 +1,27 @@
+import{BrowserRouter, Routes, Route} from 'react-router-dom';
+import Home from './pages/Home';
+import Meal from './pages/Meal'
+import NotFound from './pages/NotFound';
+import './App.css'
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Home/>} />
+        <Route path='/meal/:id' element={<Meal/>} />
+        <Route path='*' element={<NotFound/>} />
+        <Route
+        path='/admin' 
+        element={
+          <ProtectedRoute>
+            <Admin />
+          </ProtectedRoute>
+        }>
+          </Route>
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
---
+++ MealsAppRoutingStart/src/pages/Admin.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+function Admin(){
+    return(
+        export default function ProtectedRoute({children}){
+            if (!isAuthenticated)
+        }
+    )
+}
